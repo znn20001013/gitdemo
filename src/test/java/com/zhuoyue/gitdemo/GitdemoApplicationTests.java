85fa2dddd2c8d503007e39f72984f6bf6691a937
Znn <znn20001013@163.com>
2021-08-27 18:25:52 +0800

    create project

---
+++ src/test/java/com/zhuoyue/gitdemo/GitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuoyue.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
